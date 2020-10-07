ffb96b36c5e1f06ccdd656848035521100ce7754
aryantechie21 <aryantechie21@gmail.com>
2020-10-08 01:27:24 +0530

    new commit regarding maps and some styles in the polls section

--- geolocation/geolocation.js
+++ geolocation/geolocation.js
@@ -1,6 +1,9 @@
 function initMap() {
   const overlay = document.getElementById('overlay')
   const ui = document.getElementById('ui-component')
+  const remove = document.getElementById('remove-btn')
+  const markerArr = []
+  let currentMarker
 
   let officePos = { lat: 50.438284, lng: 30.515339 };
   let map = new google.maps.Map(document.getElementById('map'), {
@@ -10,7 +13,11 @@ function initMap() {
   let markerOffice = new google.maps.Marker({
       position: officePos,
       map: map,
-      label: 'M'
+      // label: 'M',
+      icon: {
+        url: './images/google-maps.svg',
+        scaledSize: new google.maps.Size(50, 50)
+      }
   });
 
 
@@ -19,18 +26,30 @@ function initMap() {
     let marker = new google.maps.Marker({
       position: coords,
       map: map,
+      animation: google.maps.Animation.DROP
+      // draggable: true,
     })
+
+    markerArr.push(marker)
 
     // event listener for opening an overlay for a particular marker point
     marker.addListener('click',function(){
       ui.classList.remove('invisible')
+      currentMarker = marker
       overlay.classList.remove('invisible')
     })
+    // console.log(markerArr)
   }
+
+  // // sets the map on all markers in the array
+  // function setMapOnAll(mapping){
+  //   markerArr.forEach(marker =>{marker.setMap(mapping)})
+  // }
+
+  // later a function can be written for removing all the markers together
 
   google.maps.event.addListener(map, 'click' ,function(event){
     addMarker(event.latLng)
-    console.log(event.latLng);
   })
 
   // event listener for DOM elements
@@ -38,4 +57,100 @@ function initMap() {
     ui.classList.add('invisible')
     overlay.classList.add('invisible')
   })
+
+  // event listener on the remove-btn for removing the marker from the map and the array
+  remove.addEventListener('click',function(){
+    markerArr.forEach((marker,index) => {
+      if(marker.position.lat() === currentMarker.position.lat() 
+      && marker.position.lng() === currentMarker.position.lng()){
+        markerArr.splice(index,1)
+        marker.setMap(null)
+        ui.classList.add('invisible')
+        overlay.classList.add('invisible')
+      }
+    })
+  })
 }
+
+
+// styles: [
+//   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+//       {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+//       {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+//       {
+//         featureType: 'administrative.locality',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#d59563'}]
+//       },
+//       {
+//         featureType: 'poi',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#d59563'}]
+//       },
+//       {
+//         featureType: 'poi.park',
+//         elementType: 'geometry',
+//         stylers: [{color: '#263c3f'}]
+//       },
+//       {
+//         featureType: 'poi.park',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#6b9a76'}]
+//       },
+//       {
+//         featureType: 'road',
+//         elementType: 'geometry',
+//         stylers: [{color: '#38414e'}]
+//       },
+//       {
+//         featureType: 'road',
+//         elementType: 'geometry.stroke',
+//         stylers: [{color: '#212a37'}]
+//       },
+//       {
+//         featureType: 'road',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#9ca5b3'}]
+//       },
+//       {
+//         featureType: 'road.highway',
+//         elementType: 'geometry',
+//         stylers: [{color: '#746855'}]
+//       },
+//       {
+//         featureType: 'road.highway',
+//         elementType: 'geometry.stroke',
+//         stylers: [{color: '#1f2835'}]
+//       },
+//       {
+//         featureType: 'road.highway',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#f3d19c'}]
+//       },
+//       {
+//         featureType: 'transit',
+//         elementType: 'geometry',
+//         stylers: [{color: '#2f3948'}]
+//       },
+//       {
+//         featureType: 'transit.station',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#d59563'}]
+//       },
+//       {
+//         featureType: 'water',
+//         elementType: 'geometry',
+//         stylers: [{color: '#17263c'}]
+//       },
+//       {
+//         featureType: 'water',
+//         elementType: 'labels.text.fill',
+//         stylers: [{color: '#515c6d'}]
+//       },
+//       {
+//         featureType: 'water',
+//         elementType: 'labels.text.stroke',
+//         stylers: [{color: '#17263c'}]
+//       }
+
+// ]
