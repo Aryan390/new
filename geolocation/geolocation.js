eae82ab4bd31c13391559ba38e02dfdafe089adc
Aryan390 <aryantechie21@gmail.com>
2021-12-08 01:38:29 +0530

    prefinal commit

--- geolocation/geolocation.js
+++ geolocation/geolocation.js
@@ -1,30 +1,45 @@
 const distModal = document.getElementById('dist-modal')
 const distBtn = document.getElementById('calcDistance')
+const getDistBtn = document.getElementById('getDistance')
+const result = document.getElementById('result')
 const list = document.getElementById('list');
+const selectedChecks = document.getElementById('selected-points')
 
-let apiKey = 'AIzaSyBSdJwCLjXwIH13M_T988PjRQX8N2KXLP0'
-apiKey = 'AIzaSyBcLQws5tAzPKlgi9RnMFs8LR7G1y4tQ_4'
+// let apiKey = 'AIzaSyBSdJwCLjXwIH13M_T988PjRQX8N2KXLP0'
+// apiKey = 'AIzaSyBcLQws5tAzPKlgi9RnMFs8LR7G1y4tQ_4'
 
+// an array for holding all the markers
 const markerArr = []
+
+// an array to specifiy what points the user has selected for calculating distance between 2 points
+const selectedPoints = []
 
 // finally did it using a different api than the google geocoding api, but still awesome that it works, we can change the code though according to the client later, the baseline is that it works
 function reverseGeocode(coords, marker){
+  let apiString =`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.lat()}&longitude=${coords.lng()}&localityLanguage=en`
+
   console.log(coords.lat());
-  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.lat()}&longitude=${coords.lng()}&localityLanguage=en`)
+  let coordString = coords.lat() + "," + coords.lng()
+  console.log(coordString);
+  fetch(`https://api.distancematrix.ai/maps/api/geocode/json?latlng=${coordString}&key=md0mnuh2PRwthoVAuAPgrzFJE1u5P`)
     .then(res => res.json())
     .then(data => {
-      let placeArr = []
-      let place = data.localityInfo.administrative
-      place.forEach(item => {
-        placeArr.push(
-          {name: item.name, desc: item.description, code: item.isoCode}
-        )
-      })
-      markerArr.push({marker, value: placeArr })
+      // let placeArr = []
+      // let place = data.localityInfo.administrative
+      // place.forEach(item => {
+      //   placeArr.push(
+      //     {name: item.name, desc: item.description, code: item.isoCode}
+      //   )
+      // })
+      // markerArr.push({marker, value: placeArr })
       // console.log(markerArr);
+      
+      const address = data.result[0].formatted_address
+      markerArr.push({marker, value: address })
     })
     .catch(err => console.log(err))
 }
+
 
 // called by google map api script , this function is passed as a callback
 function initMap() {
@@ -108,19 +123,16 @@ function initMap() {
 function showDistanceModal(){
   distModal.classList.toggle('invisible')
 
-  if(distModal.classList.contains('invisible')) return;
+  if(distModal.classList.contains('invisible')) {
+    distBtn.innerHTML = 'Calculate Distance'    
+    return
+  }
+  distBtn.innerHTML = 'x'
 
   console.log(markerArr);
 
   markerArr.forEach(item => {
-    let labelString = ''
-    item.value.forEach(val => {
-      labelString += `${val.name}, `
-    })
-    const lat = item.marker.position.lat()
-    const lng = item.marker.position.lng()
-
-    labelString += ` ${lat} - ${lng}`
+    let labelString = item.value
 
     const inputDiv = createInput(labelString);
     list.appendChild(inputDiv)
@@ -129,7 +141,30 @@ function showDistanceModal(){
 
 // this function limits the checking of the 2 checkboxes in the whole list
 function checkList (){
-  
+  const nodeList = document.querySelectorAll('.check')
+  let truthVal = 0
+  let selectedArr = []
+  nodeList.forEach(item => {
+    if(item.checked) {
+      truthVal++
+      selectedArr.push(item.value)
+    }
+  })
+
+  // if more than 2 checkboxes are selected, then open up an error modal and remove all selects
+  if(truthVal > 2) {
+    nodeList.forEach(item => {
+      item.checked = false
+    })
+    selectedArr = []
+    console.log('please choose only upto 2 locations');
+    selectedChecks.innerHTML = ''
+    return
+  }
+
+  console.log('someting');
+  selectedChecks.innerHTML = selectedArr.join(' AND ')
+
 }
 
 function createInput(labelString) {
@@ -139,7 +174,7 @@ function createInput(labelString) {
   checkbox.addEventListener('click', checkList)
   checkbox.type = "checkbox";
   checkbox.name = "name";
-  checkbox.value = "value";
+  checkbox.value = labelString;
   checkbox.id = "id";
 
   const label = document.createElement('label')
@@ -152,94 +187,40 @@ function createInput(labelString) {
   return container
 }
 
-
 // function to calculate distance between 2 points
 function calcDistance(){
-  // https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
-  console.log('somt');
+  if(selectedChecks.innerHTML === '') return
+
+  let address = selectedChecks.innerHTML
+
+  address = address.split(' AND ')
+
+  if(address.length < 2) {
+    console.log('add 2 points to get the distance between them');
+    return
+  }
+
+  const firstAddress =address[0]
+  const secondAddress = address[1]
+
+  let origin = '51.4822656,-0.1933769'
+  let destination = '51.4994794,-0.1269979'
+  let token = 'md0mnuh2PRwthoVAuAPgrzFJE1u5P'
+
+  markerArr.forEach(item => {
+    if(firstAddress === item.value) origin = item.marker.position.lat() + "," + item.marker.position.lng()  
+    else if(secondAddress === item.value) destination = item.marker.position.lat() + "," + item.marker.position.lng()  
+  })
+
+  fetch(`https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=${token}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    let distance = data.rows[0].elements[0].distance.text
+    let duration = data.rows[0].elements[0].duration.text
+    result.innerHTML = distance + " - " + duration
+  })
 }
 
-
+getDistBtn.addEventListener('click', calcDistance)
 distBtn.addEventListener('click', showDistanceModal)
-
-// styles: [
-//   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-//       {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-//       {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-//       {
-//         featureType: 'administrative.locality',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#d59563'}]
-//       },
-//       {
-//         featureType: 'poi',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#d59563'}]
-//       },
-//       {
-//         featureType: 'poi.park',
-//         elementType: 'geometry',
-//         stylers: [{color: '#263c3f'}]
-//       },
-//       {
-//         featureType: 'poi.park',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#6b9a76'}]
-//       },
-//       {
-//         featureType: 'road',
-//         elementType: 'geometry',
-//         stylers: [{color: '#38414e'}]
-//       },
-//       {
-//         featureType: 'road',
-//         elementType: 'geometry.stroke',
-//         stylers: [{color: '#212a37'}]
-//       },
-//       {
-//         featureType: 'road',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#9ca5b3'}]
-//       },
-//       {
-//         featureType: 'road.highway',
-//         elementType: 'geometry',
-//         stylers: [{color: '#746855'}]
-//       },
-//       {
-//         featureType: 'road.highway',
-//         elementType: 'geometry.stroke',
-//         stylers: [{color: '#1f2835'}]
-//       },
-//       {
-//         featureType: 'road.highway',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#f3d19c'}]
-//       },
-//       {
-//         featureType: 'transit',
-//         elementType: 'geometry',
-//         stylers: [{color: '#2f3948'}]
-//       },
-//       {
-//         featureType: 'transit.station',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#d59563'}]
-//       },
-//       {
-//         featureType: 'water',
-//         elementType: 'geometry',
-//         stylers: [{color: '#17263c'}]
-//       },
-//       {
-//         featureType: 'water',
-//         elementType: 'labels.text.fill',
-//         stylers: [{color: '#515c6d'}]
-//       },
-//       {
-//         featureType: 'water',
-//         elementType: 'labels.text.stroke',
-//         stylers: [{color: '#17263c'}]
-//       }
-
-// ]
